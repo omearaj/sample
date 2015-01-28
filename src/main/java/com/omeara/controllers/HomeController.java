3a9d0dbe44a5c8464246c6c8434754d80b64c092
omearaj <jamie4626@yahoo.com>
2015-01-28 10:52:55 -0700

    new file

--- src/main/java/com/omeara/controllers/HomeController.java
+++ src/main/java/com/omeara/controllers/HomeController.java
@@ -6,12 +6,12 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
 	
@@ -20,7 +20,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
